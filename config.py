4609e4596199828a7719f4c9c9239be7bf80fc11
Jackie547 <holarick97@gmail.com>
2026-05-25 21:58:55 +0530

    Modify configuration values in config.py
    Updated API_ID, API_HASH, and AUTH_USERS values.

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
-    API_ID = int(os.environ.get("API_ID", "7856100"))
-    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
-    AUTH_USERS = "1411895712"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "31605369"))
+    API_HASH = os.environ.get("API_HASH", "f8b54f0c81481ebf1593adebb073d844")
+    AUTH_USERS = "8282839142"
 
